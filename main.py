ad340d8f223e72cd29a705befd69b4156f147889
jimanderssongit <62330362+jimanderssongit@users.noreply.github.com>
2022-05-25 21:32:07 +0200

    add channels_date and more
    * added channels_date() function
    * switched places between add_ci and add_channel (descriptions were switched)
    * removed all the tutorial functions

--- main.py
+++ main.py
@@ -35,50 +35,13 @@ class App:
         logging.getLogger("neo4j").addHandler(handler)
         logging.getLogger("neo4j").setLevel(level)
 
-    def create_friendship(self, person1_name, person2_name):
-        with self.driver.session() as session:
-            # Write transactions allow the driver to handle retries and transient errors
-            result = session.write_transaction(
-                self._create_and_return_friendship, person1_name, person2_name)
-            for row in result:
-                print("Created friendship between: {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
-
-    @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
-        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-
-        # To execute a Cypher Query, the query text is required along with an optional set of named parameters.
-        # The text can contain parameter placeholders that are substituted with the corresponding values at
-        # runtime. While it is possible to run non-parameterized Cypher Queries, good programming practice
-        # is to use parameters in Cypher Queries whenever possible. This allows for the caching of queries
-        # within the Cypher Engine, which is beneficial for performance.
-        # Parameter values should adhere to Cypher values.
-        query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:KNOWS]->(p2) "
-            "RETURN p1, p2"
-        )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
-        try:
-            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]}
-                    for row in result]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
-
     @staticmethod
     def get_datetime(unix_time):
         return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
 
     @staticmethod
     def get_unixtime(date_time):
-        print(date_time)
         dt_split = re.split("[-_:]", date_time)
-        print(dt_split)
         year = int(dt_split[0])
         month = int(dt_split[1])
         day = int(dt_split[2])
@@ -89,23 +52,23 @@ class App:
         unixtime = time.mktime(dt.timetuple())
         return str(int(unixtime))
 
-    def find_outgoing_contacts_between_x_and_y(self, contacting, contacted):
+    def get_direct_channels(self, contacting, contacted):
         with self.driver.session() as session:
-            # Write transactions allow the driver to handle retries and transient errors
+            print("------------------------------------------------------------------------------------------------------------------")
             print("Type         Contacting          Contacted           Start time              End time")
             print("------------------------------------------------------------------------------------------------------------------")
             result = session.read_transaction(
-                self._find_outgoing_contacts_between_x_and_y, contacting, contacted)
+                self._get_direct_channels, contacting, contacted)
 
             for row in result:
-                print("{type}       {start_node}             {end_node}            {start_time}     {end_time}".format(
+                print("{type}       {start_node}            {end_node}             {start_time}     {end_time}".format(
                     type=row["type"], start_node=row["start_node"], end_node=row["end_node"],
                     start_time=self.get_datetime(row['start_time']),
                     end_time=self.get_datetime(row['end_time'])
                 ))
 
     @staticmethod
-    def _find_outgoing_contacts_between_x_and_y(tx, p1, p2):
+    def _get_direct_channels(tx, p1, p2):
         query = (
             "MATCH (p1:POI { name: $p1})-[r]-(p2:POI {name: $p2}) "
             "RETURN r, startNode(r).name as start_node, endNode(r).name as end_node, TYPE(r) as type "
@@ -128,6 +91,7 @@ class App:
     def find_all_outgoing_contacts(self, poi):
         with self.driver.session() as session:
             # Write transactions allow the driver to handle retries and transient errors
+            print("------------------------------------------------------------------------------------------------")
             print("Contacting              Contacted                  Start time                End time")
             print("------------------------------------------------------------------------------------------------")
             result = session.read_transaction(
@@ -154,36 +118,6 @@ class App:
             logging.error("{query} raised an error: \n {exception".format(
                 query=query, exception=exception))
             raise
-
-
-    def find_person(self, person_name):
-        with self.driver.session() as session:
-            # Routing Cypher by identifying reads and writes can improve the utilization of available
-            # cluster resources. Since read servers are typically more plentiful than write servers,
-            # it is beneficial to direct read traffic to read servers instead of the write server.
-            # Doing so helps in keeping write servers available for write transactions.
-            # The driver does not parse Cypher and therefore cannot automatically determine whether
-            # a transaction is intended to carry out read or write operations.
-            # As a result, a write transaction tagged as a read will still be sent to a read server,
-            # but will fail on execution.
-            result = session.read_transaction(self._find_and_return_person, person_name)
-            for row in result:
-                # Drivers translate between application language types and the Cypher Types.
-                # To pass parameters and process results, it is important to know the basics of how Cypher
-                # works with types and to understand how the Cypher Types are mapped in the driver.
-                # All types can be potentially found in the result, although not all types can be used as parameters.
-                # https://neo4j.com/docs/python-manual/current/cypher-workflow/#python-driver-type-mapping
-                print("Found person: {row}".format(row=row))
-
-    @staticmethod
-    def _find_and_return_person(tx, person_name):
-        query = (
-            "MATCH (p:Person) "
-            "WHERE p.name = $person_name "
-            "RETURN p.name AS name"
-        )
-        result = tx.run(query, person_name=person_name)
-        return [row["name"] for row in result]
 
     def add_ci(self, relationship, contacting, contacted, start, end):
         start = App.get_unixtime(start)
@@ -220,6 +154,42 @@ class App:
             result = session.write_transaction(self._create_and_return_poi, name)
             for row in result:
                 print("Created POI: {row}".format(row=row))
+
+    def get_channels_date(self, from_date_time, to_date_time):
+        from_date_time = App.get_unixtime(from_date_time)
+        to_date_time = App.get_unixtime(to_date_time)
+        with self.driver.session() as session:
+            result = session.read_transaction(self._get_channels_date, from_date_time, to_date_time)
+        print("------------------------------------------------------------------------------------------------------------------")
+        print("Type         Contacting          Contacted           Start time              End time")
+        print("------------------------------------------------------------------------------------------------------------------")
+        for row in result:
+            print("{type}       {start_node}            {end_node}             {start_time}     {end_time}".format(
+                type=row["type"], start_node=row["start_node"], end_node=row["end_node"],
+                start_time=self.get_datetime(row['start_time']),
+                end_time=self.get_datetime(row['end_time'])
+            ))
+
+    @staticmethod
+    def _get_channels_date(tx, from_date_time, to_date_time):
+        query = (
+            "MATCH (n1)-[r]->(n2)"
+            "WHERE " + from_date_time + " <= r.start_time <= " + to_date_time + " " 
+            "RETURN r, startNode(r).name as start_node, endNode(r).name as end_node, TYPE(r) as type "
+            "ORDER BY r.start_time"
+        )
+        result = tx.run(query, from_date_time=from_date_time, to_date_time=to_date_time)
+        try:
+            return [{"type": row["type"],
+                     "start_node": row["start_node"],
+                     "end_node": row["end_node"],
+                     "start_time": row["r"]["start_time"],
+                     "end_time": row["r"]["end_time"]}
+                    for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception".format(
+                query=query, exception=exception))
+            raise
 
     @staticmethod
     def _create_and_return_poi(tx, name):
@@ -269,16 +239,16 @@ class CommandLine:
                     self.add_poi(user_input)
                 elif user_input.startswith('!update_poi '):
                     self.update_poi(user_input)
-                elif user_input.startswith('!add_ci '):
-                    self.add_ci(user_input)
+                elif user_input.startswith('!add_channel '):
+                    self.add_channel(user_input)
                 elif user_input.startswith('!outgoing '):
                     self.outgoing_channels(user_input)
                 elif user_input.startswith('!communicated_with '):
                     self.communicated_with(user_input)
                 elif user_input.startswith('!comm_between '):
                     self.communication_between(user_input)
-                elif user_input.startswith('!add_channel '):
-                    self.add_channel(user_input)
+                elif user_input.startswith('!add_ci '):
+                    self.add_ci(user_input)
                 elif user_input.startswith('!direct_channels '):
                     self.direct_channels(user_input)
                 elif user_input.startswith('!channels_date '):
@@ -308,7 +278,7 @@ class CommandLine:
         pass
 
     @staticmethod
-    def add_ci(user_input):
+    def add_channel(user_input):
         # 1.3 The operator can enter a CI between operators
         channel, = user_input.split(' ')[1:]
         print(f'adding {channel} to the database')
@@ -333,10 +303,9 @@ class CommandLine:
         # The query returns the shortest path, including CI and intermediate POI’s.
         poi1, poi2 = user_input.split(' ')[1:]
         print(f'has {poi1} and {poi2} communicated?')
-        app.find_outgoing_contacts_between_x_and_y(poi1, poi2)
 
     @staticmethod
-    def add_channel(user_input):
+    def add_ci(user_input):
         # 1.7 The operator can add a new CI between two existing POIs.
         poi1, channel, poi2, start, end = user_input.split(' ')[1:]
         print(f'adding {channel} between {poi1} and {poi2} with start time {start} and end time {end}')
@@ -347,12 +316,14 @@ class CommandLine:
         # 1.8 The operator can get all CIs between two directly communicating POIs.
         poi1, poi2 = user_input.split(' ')[1:]
         print(f'finding the direct channels between {poi1} and {poi2}')
+        app.get_direct_channels(poi1, poi2)
 
     @staticmethod
     def channels_date(user_input):
         # 1.9 The operator can get all CIs between two points of datetime.
-        date1, date2 = user_input.split(' ')[1:]
-        print(f'finding channels between {date1} and {date2}')
+        from_datetime, to_datetime = user_input.split(' ')[1:]
+        print(f'finding channels between {from_datetime} and {to_datetime}')
+        app.get_channels_date(from_datetime, to_datetime)
 
     @staticmethod
     def add_poi(user_input):
